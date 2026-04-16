156e9e38d5e8229d7f663129f7241f06fa04350a
Rafael <v.rafael02@edu.pucrs.br>
2026-04-15 22:19:12 -0300

    1.0.1

--- threadmutex.c
+++ threadmutex.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 int count = 0;
 
 void* increase(){
@@ -10,6 +11,15 @@ void* increase(){
 
 int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
+
+    pthread_t thread[n];
+    for (int i=0; i<n; i++){
+        pthread_create(&thread[i], NULL, increase, NULL);
+    }
+
+    for (int i=0; i<n; i++){
+        pthread_join(thread[i], NULL);
+    }
 
     printf("%d", count);
     return 0;
