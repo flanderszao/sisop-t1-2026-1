0e8b4e55967b48cace6b6a752e055c6b3ad52cad
Flanders <v.rafael02@edu.pucrs.br>
2026-04-22 21:32:14 -0300

    iniciando processos

---
+++ auto.sh
@@ -0,0 +1,9 @@
+for i in {1..10}; do
+    ./a.out 2
+done
+for i in {1..10}; do
+    ./a.out 4
+done
+for i in {1..10}; do
+    ./a.out 8
+done
--- p1.c
+++ p1.c
@@ -9,19 +9,37 @@ FILE *fptr;
 
 void* increase(){
     count++;
+    return NULL;
 }
 
 int main(int argc, char *argv[]){
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <num_forks>\n", argv[0]);
+        return 1;
+    }
+
     int n = atoi(argv[1]);
     clock_t inicio = clock();
-   
+    int buf, one = 1, fd[2];
+    pipe(fd);
+
     for (int i = 0; i < n; i++) {
         pid_t pid = fork();
         if (pid == 0) {
             //ADICIONAR LÓGICA
-            // Child process
+            read(fd[0], &buf, sizeof(buf));
+            count = buf;
+            while (count < 1000000000){
+                write(fd[1], &one, sizeof(one));
+            }
             //printf("Child %d created (PID: %d)\n", i + 1, getpid());
             return 0;
+        } else {
+            while (count < 1000000000){
+            read(fd[0], &buf, sizeof(buf));
+            count = count + buf;
+            }
+            // Parent process
         }
     }
 
@@ -30,9 +48,9 @@ int main(int argc, char *argv[]){
 
     printf("Tempo: %.6f s\n", tempo);
 
-    fptr = fopen("thread.txt", "a");
+    fptr = fopen("fork.txt", "a");
 
-    fprintf(fptr, "\nEm %d Threads: %d | Tempo: %.6f s", n, count, tempo);
+    fprintf(fptr, "\nEm %d Forks: %d | Tempo: %.6f s", n, count, tempo);
 
     fclose(fptr);
 
